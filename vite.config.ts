c355679cee5f43f8cae2eecbfbb2861d78d6e602
tobilobacodes <tobilobasulaimonabayomi@gmail.com>
2026-02-27 11:41:17 +0100

    final changes I hope

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     tanstackStart({ srcDirectory: './app' }),
+    nitro(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
